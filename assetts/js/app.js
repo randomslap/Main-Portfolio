fc349194758ea668378fd8a807efb20e06688de2
randomslap <kentokazaki@gmail.com>
2019-05-15 18:34:15 -0700

    added font awesome icons; fixed mobile response

--- assetts/js/app.js
+++ assetts/js/app.js
@@ -1,1 +1,24 @@
 
+$(document).ready(function() {
+	sticky();
+
+});
+
+function sticky() {
+	$(".ui.sticky").sticky({
+		context: "#2",
+	});
+	if ($(".ui.sticky").hasClass("fixed")) {
+		console.log("menu is sticky");
+	}
+}
+
+function fullPage() {
+	$("#fullpage").fullpage({
+		autoScrolling: false,
+		scrollHorizontally: false,
+	});
+
+	$.fn.fullpage.setAllowScrolling(false);
+}
+
